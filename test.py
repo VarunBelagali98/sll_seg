3ed362e6660f69e7b8064f1577ec71a1fba8a049
varunb <varun.belagali@citrix.com>
2022-01-19 11:30:46 +0530

    test ReNet

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import torch
 import torchvision
 import torch.optim as optim
-from dataloader import load_data
+#from dataloader import load_data
+from dataloaders.ReNet_dataloader import load_data
 from torch.utils import data as data_utils
 from tqdm import tqdm
 from torchsummary import summary
@@ -9,7 +10,8 @@ import argparse
 import numpy as np 
 import cv2
 import os
-from models.DsNet import DSNet
+from models.ReNet import ReNet
+import matplotlib.pyplot as plt 
 
 use_cuda = torch.cuda.is_available()
 
@@ -43,20 +45,23 @@ def save_samples(test_data_loader, device, model):
 		# Move data to CUDA device
 		x1, x2 = data[0], data[1]
 		x1 = x1.to(device)
+		x2 = x2.to(device)
 
-		pred, _ = model.forward(x1)
+		pred, _ = model.forward(x1, x2)
 
 		x1 = x1.cpu().detach().numpy()
 		pred = pred.cpu().detach().numpy()
-		pred = pred > (1.0 / (pred.shape[-1] * pred.shape[-2]))
+		#pred = pred > (1.0 / (pred.shape[-1] * pred.shape[-2]))
+		pred = np.argmax(pred, axis=1)
 		pred = pred.astype(int)
 
 		for i in range(x1.shape[0]):
 			img = x1[i, 0, :, :] * 255
 			img = img.astype(int)
-			seg = pred[i, 0, :, :] * 255
+			seg = pred[i, :, :] #pred[i, 0, :, :] * 255
 			cv2.imwrite('./samples/'+str(count)+".png", img)
-			cv2.imwrite('./samples/'+str(count)+"_seg.png", seg)
+			#cv2.imwrite('./samples/'+str(count)+"_seg.png", seg)
+			plt.imsave('./samples/'+str(count)+"_seg.png", seg)
 			count = count + 1
 		
 		if count > 100:
@@ -67,14 +72,15 @@ if __name__ == "__main__":
 	model_name = args.model_name
 	batch_size = args.batch_size
 	TRAINING_PATH = args.root_data
-	FOLD_PATH = args.fold_data + "fold_struct/fold"
+	FOLD_PATH = args.fold_data + 'fold_files/annfiles_fold' #"fold_struct/fold"
 	ROOT_WEIGHTPATH = args.weight_root
 	Vid_to_IMG_PATH = args.fold_data + "videoId_to_imgIdx/"
 	
 	WEIGTH_PATH = ROOT_WEIGHTPATH + model_name + ".pth"
 
 	# Dataset and Dataloader setup
-	test_dataset = load_data(fold, 2, TRAINING_PATH=TRAINING_PATH, FOLD_PATH=FOLD_PATH, Vid_to_IMG_PATH=Vid_to_IMG_PATH)
+	#test_dataset = load_data(fold, 2, TRAINING_PATH=TRAINING_PATH, FOLD_PATH=FOLD_PATH, Vid_to_IMG_PATH=Vid_to_IMG_PATH)
+	test_dataset = load_data(fold, 2, TRAINING_PATH=TRAINING_PATH, FOLD_PATH=FOLD_PATH)
 
 	test_data_loader = data_utils.DataLoader(
 		test_dataset, batch_size=batch_size)
@@ -82,8 +88,8 @@ if __name__ == "__main__":
 	device = torch.device("cuda" if use_cuda else "cpu")
 
 	# Model
-	model = DSNet().to(device)
-	summary(model, (1, 224, 224))
+	model = ReNet().to(device)
+	#summary(model, (1, 224, 224))
 
 	# Load weights
 	model.load_state_dict(torch.load(WEIGTH_PATH))
